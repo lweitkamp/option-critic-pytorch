c26e5ea148bd7d6559b857774a5d32f145db6488
lweitkamp <laurens.weitkamp@student.uva.nl>
2020-03-22 10:06:51 +0100

    adding epsilon-greedy schedule to utils

--- utils.py
+++ utils.py
@@ -2,49 +2,32 @@ import gym
 import numpy as np
 import torch
 
-from gym.wrappers import AtariPreprocessing, TransformReward
-from gym.wrappers import FrameStack as FrameStack_
-
 from fourrooms import Fourrooms
 
 
-class LazyFrames(object):
-    def __init__(self, frames):
-        self._frames = frames
+class LinearSchedule:
+    """
+    Linear scheduler, for epsilon-greedy option selection.
+    """
+    def __init__(self, e_start=1.0, e_min=0.1, e_decay=int(1e6)):
+        self.num_steps = 0
+        self.e_min   = e_min
+        self.e_start = e_start
+        self.e_decay = e_decay
 
-    def __array__(self, dtype=None):
-        out = np.concatenate(self._frames, axis=0)
-        if dtype is not None:
-            out = out.astype(dtype)
-        return out
+    def __next__(self):
+        epsilon = self.e_min + (self.e_start - self.e_min) * np.exp(-self.num_steps / self.e_decay)
+        self.num_steps += 1
+        return epsilon
 
-    def __len__(self):
-        return len(self.__array__())
-
-    def __getitem__(self, i):
-        return self.__array__()[i]
-
-
-class FrameStack(FrameStack_):
-    def __init__(self, env, k):
-        FrameStack_.__init__(self, env, k)
-
-    def _get_ob(self):
-        assert len(self.frames) == self.k
-        return LazyFrames(list(self.frames))
 
 def make_env(env_name):
 
     if env_name == 'fourrooms':
-        return Fourrooms(), False
+        env = Fourrooms()
 
     env = gym.make(env_name)
-    is_atari = hasattr(gym.envs, 'atari') and isinstance(env.unwrapped, gym.envs.atari.atari_env.AtariEnv)
-    if is_atari:
-        env = AtariPreprocessing(env, grayscale_obs=True, scale_obs=True, terminal_on_life_loss=True)
-        env = TransformReward(env, lambda r: np.clip(r, -1, 1))
-        env = FrameStack(env, 4)
-    return env, is_atari
+    return env
 
 def to_tensor(obs):
     obs = np.asarray(obs)
