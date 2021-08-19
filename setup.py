b410ef1690ab25e25191f3858ea10e53592deab8
laurens <laurens@MBP-van-laurens.home>
2021-08-19 18:38:43 +0200

    adding gym requirement

--- setup.py
+++ setup.py
@@ -3,4 +3,4 @@ from setuptools import setup
 setup(
    name='option_critic_pytorch',
    packages=['option_critic_pytorch'],
-   install_requires=['numpy', 'supersuit', 'gym', 'torch'])
+   install_requires=['numpy', 'supersuit', 'gym>=0.18.0', 'torch'])
