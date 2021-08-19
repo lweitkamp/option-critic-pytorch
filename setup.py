b12fa35d44618c83cabb72ac5d6ef4e09200dd82
laurens <laurens@MBP-van-laurens.home>
2021-08-19 18:28:07 +0200

    adding supersuit to setup

--- setup.py
+++ setup.py
@@ -3,4 +3,4 @@ from setuptools import setup
 setup(
    name='option_critic_pytorch',
    packages=['option_critic_pytorch'],
-   install_requires=['numpy', 'gym', 'torch'])
+   install_requires=['numpy', 'gym', 'supersuit', 'torch'])
