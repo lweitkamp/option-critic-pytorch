81945041abc128e38694b9425965e998b6844999
laurensweitkamp <laurensweitkamp@thirona.eu>
2024-07-31 18:28:08 +0200

    add time limit

--- four_rooms/__init__.py
+++ four_rooms/__init__.py
@@ -3,5 +3,5 @@ from gymnasium.envs.registration import register
 register(
      id="four_rooms/FourRooms-v0",
      entry_point="four_rooms.envs:FourRoomsEnv",
-     # max_episode_steps=1000,
+     max_episode_steps=1000,
 )
